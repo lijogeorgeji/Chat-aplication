198ac46e81fc1086fe2e6a6d30671f81607c6174
lijogeorgeji <lijogeorge773@gmail.com>
2021-02-06 19:10:12 +0530

    Android chat app

---
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.chat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference myDatabase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
+
+        TextView myText = findViewById(R.id.conver);
+
+
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                myText.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+               myText.setText("CANCELLED");
+            }
+        });
+    }
+
+    public void sendMessage(View view){
+        EditText myEditText = findViewById(R.id.chat);
+
+        myDatabase.child("User").setValue(myEditText.getText().toString());
+        myEditText.setText("");
+    }
+
+
+
+}
